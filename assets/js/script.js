490fc302546944b51f89b3b22e2d8d17bcd5e8fb
WilsonZhu <winniecmz.zhu@gmail.com>
2022-02-05 11:50:18 -0500

    display api contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+// import api key for Abstract API
+var apiKeyTel1="67fbd798cbc74fddaee6c95cf604308a";
+// import api key for numberify API
+var apiKeyTel2="bdc7f61860e05e5ba773ad3b44017274";
+// import api key for MapQuest API
+var apiKeyMap="jDRCcpslgqlzGKPpjn9f6ElnCMI8W49W";
+
 var telForm = document.getElementById("phone-form"),
     telInput = document.getElementById("phone"),
     formMessage = document.getElementById("form-message");
@@ -5,7 +12,6 @@ var telForm = document.getElementById("phone-form"),
 // form handler
 
 var searchForm = function(event){
-    event.preventDefault();
     formMessage.textContent = "Searching..."
     formMessage.style = "color:black";
     var phoneNumber = telInput.value.trim();
@@ -22,21 +28,25 @@ var searchForm = function(event){
         return null;
     }
 
+    // Dispaly the phone number at the "your search result....."
+    phoneNumDisplay(phoneNumber);
+
     //call tel api 1
     var apiUrl = "https://phonevalidation.abstractapi.com/v1/?api_key=" + apiKeyTel1 + "&phone=" + phoneNumber;
 
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
-            if(data.registered_location){
+            if(data.location){
                 //if api 1 returns the location, proceed, else try 2nd api
-                var telValid = data.is_valid_number,
-                    telCountry = data.country_code,
-                    telPrefix = data.country_prefix,
-                    telLocation = data.registered_location,
-                    telType = data.line_type,
+                var telValid = data.valid,
+                    telCountry = data.country.name,
+                    telPrefix = data.country.prefix,
+                    telLocation = data.location,
+                    telType = data.type,
                     telCarrier = data.carrier;
                 console.log("API1:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
-                // createResultInfo(phoneNumber,telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
+                // phone info display
+                phoneInfoDisplay(telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
             } else {
                 //call tel api 2
                 var apiUrl2 = "http://apilayer.net/api/validate?access_key=" + apiKeyTel2 + "&number=" + phoneNumber;
@@ -48,8 +58,9 @@ var searchForm = function(event){
                             telLocation = data2.telLocation,
                             telType = data2.line_type,
                             telCarrier = data2.carrier;
-                        console.log("API2:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
-                        // createResultInfo(phoneNumber,telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
+                        // console.log("API2:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
+                        // phone info display
+                        phoneInfoDisplay(telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
                     })
                 }).catch(function(error){
                     formMessage.textContent = "Connection error!";
@@ -61,12 +72,29 @@ var searchForm = function(event){
             } else {
                 var apiUrl3 = "https://www.mapquestapi.com/staticmap/v5/map?key=" + apiKeyMap + "&center=" + telCountry + "&zoom=5&size=@2x";
             }
-           //display map function here using apiURL3 as the image
+           
+            //display map function here using apiURL3 as the image
+            mapDisplay(apiUrl3);
+
         });
     }).catch(function(error){
         formMessage.textContent = "Connection error!";
         formMessage.style = "color:red";
     });
+    
+}
+
+var mapDisplay=function(mapURL){
+    // refresh the map
+    $(".map").remove();
+
+    var mapEl=$("<iframe>")
+        .addClass("map")
+        .attr("src",mapURL,
+            "alt","location_map",
+            "style","boarder:0;");
+
+    $(".contact-info").append(mapEl);
 }
 
 // data={
@@ -83,55 +111,74 @@ var searchForm = function(event){
 //     "type":"mobile",
 //     "carrier":"T-Mobile Usa, Inc."}
 
-var creatResultInfo=function(validResponse){
+var creatResultInfo=function(validResponse,telCountry,telPrefix,telLocation,telType,telCarrier){
+      
+    var resultListEl=$("<ul>")
+        .addClass("result-list")
+    
     var validEl=$("<li>")
         .addClass("phone-valid")
-        .text(validResponse);
+        .text("Valid: "+validResponse);
         console.log(validEl)
     
     var countryEl=$("<li>")
         .addClass("phone-country")
-        .text("Country: "+data.country.name);
+        .text("Country: "+telCountry);
     
     var countryPrefixEl=$("<li>")
         .addClass("phone-countryPrefix")
-        .text("Country Prefix: "+data.country.prefix);
+        .text("Country Prefix: "+telPrefix);
     
     var localtionEl=$("<li>")
         .addClass("phone-location")
-        .text("Location: "+data.location);
+        .text("Location: "+telLocation);
     
     var typeEl=$("<li>")
         .addClass("phone-type")
-        .text("Type: "+data.type);
+        .text("Type: "+telType);
 
     var carrierEl=$("<li>")
         .addClass("phone-carrier")
-        .text("Carrier: "+data.carrier);
+        .text("Carrier: "+telCarrier);
     
     $(".result-box")
-        .append(validEl,
-            countryEl,
-            countryPrefixEl,
-            localtionEl,
-            typeEl,
-            carrierEl);
+    .append(resultListEl);
+        
+    $(".result-list")
+    .append(validEl,
+        countryEl,
+        countryPrefixEl,
+        localtionEl,
+        typeEl,
+        carrierEl);
 }
 
-$(".search-btn").click(function(){
+// Display the input phone number
+var phoneNumDisplay=function(phoneNumber){
+    $(".middle-history")
+        .find("h4")
+        .text("Your result for "+phoneNumber);
+}
+
+// enpower the results after pressing the search button
+var phoneInfoDisplay=function(telValid,telCountry,telPrefix,telLocation,telType,telCarrier){
+    if (telValid==true){
+        var validResponse="This is a valid phone number";
+    }
+    else{
+        var validResponse="This is a FAKE phone number";
+    };
+
+    creatResultInfo(validResponse,telCountry,telPrefix,telLocation,telType,telCarrier)
+}
+
+//event listener
+telForm.addEventListener("submit",function(){
     event.preventDefault();
     console.log("button clicked");
+
+    // reset the display box to prevent dulicate display.
+    $(".result-list").remove();
+
+    searchForm();
 });
-
-$(".middle-history")
-    .find("h4")
-    .text("Your result for "+"14152007986");
-
-if (data.valid==true){
-    var validResponse="This is a valid phone number";
-}
-else{
-    var validResponse="This is a FAKE phone number";
-};
-
-creatResultInfo(validResponse);
