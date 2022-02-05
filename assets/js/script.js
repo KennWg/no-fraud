cb974b72672f8b46f1fbd9ea316e3eb2ec28aa47
SophieQuan <sophiequan7@gmail.com>
2022-02-04 22:50:06 -0500

    create save-function branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,19 @@ var telForm = document.getElementById("phone-form"),
 
 // form handler
 
-var searchForm = function(event){
+var searchForm = function(event) {
     event.preventDefault();
     formMessage.textContent = "Searching..."
     formMessage.style = "color:black";
     var phoneNumber = telInput.value.trim();
-    
+
     //check if phone number is a number and if the length is from 6-15 digits
-    if(isNaN(phoneNumber)){
+    if (isNaN(phoneNumber)) {
         formMessage.textContent = "Please use only numerals in your phone number.";
         formMessage.style = "color:red";
         return null;
     }
-    if(phoneNumber.length < 6 || phoneNumber.length > 15){
+    if (phoneNumber.length < 6 || phoneNumber.length > 15) {
         formMessage.textContent = "Invalid telephone number length.";
         formMessage.style = "color:red";
         return null;
@@ -25,9 +25,9 @@ var searchForm = function(event){
     //call tel api 1
     var apiUrl = "https://phonevalidation.abstractapi.com/v1/?api_key=" + apiKeyTel1 + "&phone=" + phoneNumber;
 
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            if(data.registered_location){
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            if (data.registered_location) {
                 //if api 1 returns the location, proceed, else try 2nd api
                 var telValid = data.is_valid_number,
                     telCountry = data.country_code,
@@ -40,8 +40,8 @@ var searchForm = function(event){
             } else {
                 //call tel api 2
                 var apiUrl2 = "http://apilayer.net/api/validate?access_key=" + apiKeyTel2 + "&number=" + phoneNumber;
-                fetch(apiUrl2).then(function(response2){
-                    response2.json().then(function(data2){
+                fetch(apiUrl2).then(function(response2) {
+                    response2.json().then(function(data2) {
                         var telValid = data2.valid,
                             telCountry = data2.country_code,
                             telPrefix = data2.country_prefix,
@@ -51,23 +51,23 @@ var searchForm = function(event){
                         console.log("API2:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
                         // createResultInfo(phoneNumber,telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
                     })
-                }).catch(function(error){
+                }).catch(function(error) {
                     formMessage.textContent = "Connection error!";
                     formMessage.style = "color:red";
                 });
             }
-            if(telLocation){
-            var apiUrl3 = "https://www.mapquestapi.com/staticmap/v5/map?key=" + apiKeyMap + "&center=" + telLocation + "&size=@2x";
+            if (telLocation) {
+                var apiUrl3 = "https://www.mapquestapi.com/staticmap/v5/map?key=" + apiKeyMap + "&center=" + telLocation + "&size=@2x";
             } else {
                 var apiUrl3 = "https://www.mapquestapi.com/staticmap/v5/map?key=" + apiKeyMap + "&center=" + telCountry + "&zoom=5&size=@2x";
             }
-           //display map function here using apiURL3 as the image
+            //display map function here using apiURL3 as the image
         });
-    }).catch(function(error){
+    }).catch(function(error) {
         formMessage.textContent = "Connection error!";
         formMessage.style = "color:red";
     });
 }
 
 //event listener
-telForm.addEventListener("submit",searchForm);
+telForm.addEventListener("submit", searchForm);
