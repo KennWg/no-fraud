656b1195af50baac203f9da9c3ecdc1cdb920fcc
WilsonZhu <winniecmz.zhu@gmail.com>
2022-02-06 13:08:40 -0500

    compact to the new ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,36 +7,36 @@ var apiKeyMap="jDRCcpslgqlzGKPpjn9f6ElnCMI8W49W";
 
 var telForm = document.getElementById("phone-form"),
     telInput = document.getElementById("phone"),
-    formMessage = document.getElementById("form-message");
+    formMessage = document.getElementById("form-message"),
+    recentSearch = document.querySelector(".number-box"),
+    searchHistory = [];
 
 // form handler
 
-var searchForm = function(event){
+var searchForm = function(event) {
+    // event.preventDefault();
     formMessage.textContent = "Searching..."
     formMessage.style = "color:black";
     var phoneNumber = telInput.value.trim();
-    
+
     //check if phone number is a number and if the length is from 6-15 digits
-    if(isNaN(phoneNumber)){
+    if (isNaN(phoneNumber)) {
         formMessage.textContent = "Please use only numerals in your phone number.";
         formMessage.style = "color:red";
         return null;
     }
-    if(phoneNumber.length < 6 || phoneNumber.length > 15){
+    if (phoneNumber.length < 6 || phoneNumber.length > 15) {
         formMessage.textContent = "Invalid telephone number length.";
         formMessage.style = "color:red";
         return null;
     }
 
-    // Dispaly the phone number at the "your search result....."
-    phoneNumDisplay(phoneNumber);
-
     //call tel api 1
     var apiUrl = "https://phonevalidation.abstractapi.com/v1/?api_key=" + apiKeyTel1 + "&phone=" + phoneNumber;
 
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            if(data.location){
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            if (data.location) {
                 //if api 1 returns the location, proceed, else try 2nd api
                 var telValid = data.valid,
                     telCountry = data.country.name,
@@ -45,49 +45,139 @@ var searchForm = function(event){
                     telType = data.type,
                     telCarrier = data.carrier;
                 console.log("API1:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
-                // phone info display
+                // createResultInfo(phoneNumber,telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
                 phoneInfoDisplay(telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
             } else {
                 //call tel api 2
                 var apiUrl2 = "http://apilayer.net/api/validate?access_key=" + apiKeyTel2 + "&number=" + phoneNumber;
-                fetch(apiUrl2).then(function(response2){
-                    response2.json().then(function(data2){
-                        var telValid = data2.valid,
-                            telCountry = data2.country_code,
-                            telPrefix = data2.country_prefix,
-                            telLocation = data2.telLocation,
-                            telType = data2.line_type,
-                            telCarrier = data2.carrier;
-                        // console.log("API2:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
-                        // phone info display
+                fetch(apiUrl2).then(function(response2) {
+                    response2.json().then(function(data2) {
+                        //check if api2 returns valid response
+                        if (data2.valid) {
+                            var telValid = data2.valid,
+                                telCountry = data2.country_name,
+                                telPrefix = data2.country_prefix,
+                                telLocation = data2.location,
+                                telType = data2.line_type,
+                                telCarrier = data2.carrier;
+                            console.log("API2:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
+                        } else if (data.valid) {
+                            var telValid = data.valid,
+                                telCountry = data.country.name,
+                                telPrefix = data.country.prefix,
+                                telLocation = data.location,
+                                telType = data.type,
+                                telCarrier = data.carrier;
+                            console.log("API1:" + telValid + telCountry + telPrefix + telLocation + telType + telCarrier);
+                        } else {
+                            formMessage.textContent = "Invalid telephone number.";
+                            formMessage.style = "color:red";
+                            return null;
+                        }
+                        // createResultInfo(phoneNumber,telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
                         phoneInfoDisplay(telValid,telCountry,telPrefix,telLocation,telType,telCarrier);
                     })
-                }).catch(function(error){
+                }).catch(function(error) {
                     formMessage.textContent = "Connection error!";
                     formMessage.style = "color:red";
                 });
             }
-            if(telLocation){
+            if (telLocation) {
                 var apiUrl3 = "https://www.mapquestapi.com/staticmap/v5/map?key=" + apiKeyMap + "&center=" + telLocation + "&size=@2x";
             } else {
                 var apiUrl3 = "https://www.mapquestapi.com/staticmap/v5/map?key=" + apiKeyMap + "&center=" + telCountry + "&zoom=5&size=@2x";
             }
-           
+            
             //display map function here using apiURL3 as the image
             mapDisplay(apiUrl3);
-
         });
-    }).catch(function(error){
+    }).catch(function(error) {
         formMessage.textContent = "Connection error!";
         formMessage.style = "color:red";
     });
-    
-}
+    //add new search to recent search box
+    historyList();
+};
+
+//function save history after a search occurs
+function historyList() {
+    var searchedTel = telInput.value.trim();
+    //condition avoid repeat # on the array
+    if (!searchHistory.includes(searchedTel)) {
+        //put new search phone number on search history array
+        searchHistory.push(searchedTel);
+
+        var displayRecentSearch = $(`
+            <ul>
+                <li class="numList">${searchedTel}</li>
+            </ul>
+        `);
+
+        $(".number-box").append(displayRecentSearch);
+
+    }
+    console.log(searchHistory);
+
+    //saved recent search
+    localStorage.setItem("searchedTel", JSON.stringify(searchHistory));
+
+    //reset input field after press button or enter
+    searchedTel.value = ""
+};
+
+function loadHistory(lastIndex) {
+    searchHistory = JSON.parse(localStorage.getItem("searchedTel"));
+    console.log(searchHistory);
+
+    if (!searchHistory) {
+        searchHistory = [];
+        return false;
+    } else if (searchHistory.length > 5) {
+        //store max 5 recent search
+        searchHistory.shift();
+    }
+
+    //when user click on the recent searched list, load the data 
+    var histotyList = document.createElement('ul');
+    histotyList.className = 'numList';
+    recentSearch.appendChild(histotyList);
+
+    for (var i = 0; i < searchHistory.length; i++) {
+        var recentNumSearched = document.createElement('li');
+        recentNumSearched.setAttribute('value', searchHistory[i]);
+        recentNumSearched.textContent = searchHistory[i];
+        histotyList.prepend(recentNumSearched);
+    }
+
+    var clickedNum = document.querySelector('.numList');
+
+    //for the last search
+    recentSearch.addEventListener('click', recentNumber);
+    //for clicking on other recent search list
+    clickedNum.addEventListener('click', recentNumber);
+
+    loadLatestSearch();
+};
+
+//function load the lastest search on the screen
+var loadLatestSearch = function() {
+    //get the lasted search
+    if (historyList !== null) {
+        var lastSearchedIndex = historyList.length - 1;
+        var lastSearchedCity = historyList[lastSearchedIndex];
+    }
+};
+
+//function get value on the recent # put on the recent search list
+var recentNumber = function(event) {
+    var selectedNum = event.target.getAttribute('value');
+};
+
+//displays the loaded history when the page first loads
+loadHistory();
 
 var mapDisplay=function(mapURL){
-    // refresh the map
-    $(".map").remove();
-
+        
     var mapEl=$("<iframe>")
         .addClass("map")
         .attr("src",mapURL,
@@ -98,20 +188,6 @@ var mapDisplay=function(mapURL){
 
     $(".contact-info").append(mapEl);
 }
-
-// data={
-//     "phone":"14152007986",
-//     "valid":true,
-//     "format":{
-//         "international":"+14152007986",
-//         "local":"(415) 200-7986"},
-//     "country":{
-//         "code":"US",
-//         "name":"United States",
-//         "prefix":"+1"},
-//     "location":"California",
-//     "type":"mobile",
-//     "carrier":"T-Mobile Usa, Inc."}
 
 var creatResultInfo=function(validResponse,telCountry,telPrefix,telLocation,telType,telCarrier){
       
@@ -158,8 +234,8 @@ var creatResultInfo=function(validResponse,telCountry,telPrefix,telLocation,telT
 // Display the input phone number
 var phoneNumDisplay=function(phoneNumber){
     $(".middle-history")
-        .find("h4")
-        .text("Your result for "+phoneNumber);
+        .find("result-num")
+        .text(phoneNumber);
 }
 
 // enpower the results after pressing the search button
@@ -174,33 +250,22 @@ var phoneInfoDisplay=function(telValid,telCountry,telPrefix,telLocation,telType,
     creatResultInfo(validResponse,telCountry,telPrefix,telLocation,telType,telCarrier)
 }
 
-// Storage the data
-function dateStorage(phoneNumber){
-    localStorage.setItem(5,phoneNumber)
-}
-
-//Load the data
-function dataLoading(){
-    for (var i=0;i<5;i++){
-        var id="#"+i;
-        console.log(id)
-        var historyNum=JSON.parse(localStorage.getItem(i));
-        $(id).text(historyNum);
-    }
-}
-
 //event listener
-telForm.addEventListener("submit",function(){
+telForm.addEventListener("submit", function(){
     event.preventDefault();
-    // dataLoading()
 
-    console.log("button clicked");
+    // remove the previous map
+    $(".map").remove();
 
-    var phoneNumber = telInput.value.trim();
-    // dateStorage(phoneNumber);
-
-    // reset the display box to prevent dulicate display.
+    // remove the previous searching results
     $(".result-list").remove();
-
+      
     searchForm();
+});
+
+// function when user press enter key
+telInput.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        telForm.click();
+    }
 });
